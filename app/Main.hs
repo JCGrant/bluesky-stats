7452d23543a9311241a36d9e60221553bcc2db73
JCGrant <james@jcgrant.com>
2024-11-30 16:59:46 +0000

    add some styling with tailwind

--- app/Main.hs
+++ app/Main.hs
@@ -42,21 +42,31 @@ getUserR username = do
       followerCountEntries <- runDB $ selectList [FollowerCountEntryUserId ==. entityKey user] [Desc FollowerCountEntryTimestamp]
       defaultLayout $ do
         setTitle $ toHtml username <> "'s profile"
+        toWidgetHead [hamlet|<script src="https://cdn.tailwindcss.com"></script>|]
+        toWidgetHead
+          [cassius|
+            body
+              background-color: #020617
+              color: #cbd5e1
+          |]
         [whamlet|
-          <h1>#{username}
-          <p>Live follower count: <span id="live-follower-count">Fetching...</span>
-          <table>
-            <tr>
-              <th>Count
-              <th>Timestamp
-            $forall Entity _ (FollowerCountEntry _ count timestamp) <- followerCountEntries
-              <tr>
-                <td>#{count}
-                <td>#{formatUTCTime timestamp}
+          <div class="flex flex-col gap-4 p-4">
+            <h1 class="text-3xl font-bold">#{username}'s Follower Count
+            <p>Live follower count: <span id="live-follower-count">Fetching...</span>
+            <table class="w-full text-left table-auto">
+              <thead class="uppercase bg-slate-800">
+                <tr>
+                  <th scope="col" class="px-6 py-3">Time
+                  <th scope="col" class="px-6 py-3">Count
+              <tbody class="bg-slate-900">
+                $forall Entity _ (FollowerCountEntry _ count timestamp) <- followerCountEntries
+                  <tr class="border-t border-slate-800">
+                    <td class="px-6 py-4">#{formatUTCTime timestamp}
+                    <td class="px-6 py-4">#{count}
         |]
         toWidgetBody
           [julius|
-            var eventSource = new EventSource("@{LiveR username}");
+            const eventSource = new EventSource("@{LiveR username}");
             eventSource.onmessage = function(event) {
               document.getElementById("live-follower-count").innerText = event.data;
             };
